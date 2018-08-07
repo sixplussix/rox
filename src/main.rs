4e149771be20c90eabc4e49f51ace0182deef146
fuanzhao <arnkorty.fu@gmail.com>
2018-08-07 14:42:59 +0800

    copy codes

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,22 @@ use std::net::TcpListener;
 use std::net::TcpStream;
 use std::fs::File;
 
+use std::thread;
+use std::time::Duration;
+
+extern crate rox;
+use rox::ThreadPool;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-
-    for stream in listener.incoming() {
+    let pool = ThreadPool::new(4);
+    for stream in listener.incoming().take(2) {
         let stream = stream.unwrap();
-
-        handle_connection(stream);
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
+    println!("Shutting down.");
 }
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 512];
@@ -19,8 +26,12 @@ fn handle_connection(mut stream: TcpStream) {
 
     // println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
     let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
 
     let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
+    } else if buffer.starts_with(sleep){
+        thread::sleep(Duration::from_secs(5));
         ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
     } else {
         ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
